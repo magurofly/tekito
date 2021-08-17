2b79793acf6f89473b6f4a7d40b49304108b5fde
magurofly <54321436+magurofly@users.noreply.github.com>
2021-08-18 01:43:56 +0900

    Update main.js

--- main.js
+++ main.js
@@ -1,3 +1,5 @@
+const version = "ver. 0.0.5";
+
 function defineProps(dict) {
   for (const key in dict) {
     Object.defineProperty(window, key, {
@@ -9,9 +11,11 @@ function defineProps(dict) {
 }
 defineProps({
   _incrementMoney: false,
-  _buyJuice: false,
+  _buyJuice: null,
 });
 window.incrementMoney = Function("window._incrementMoney = true;");
+
+const juicePrices = {};
 
 const app = new Vue({
   el: "#app",
@@ -20,35 +24,37 @@ const app = new Vue({
     add: new Decimal(1),
     fatigue: new Decimal(1),
     state: "initial",
-  },
-  computed: {
-    message() {
-      switch (this.state) {
-        case "initial":
-          return `<h2>ようこそ</h2><p>DevToolsを開いて、コンソールから<code>incrementMoney();</code>を実行してください。</p>`;
-        case "first-click":
-          return `<h2>おめでとうございます！</h2><p>あなたはお金を稼ぎました</p>`;
-        case "buy-juice":
-          return `<h2>そろそろ疲れてきましたか？</h2><p><code>buyJuice();</code>でジュースを買うことができます。</p>`;
-        case "bought-juice":
-          return `<h2>ああ、おいしい！</h2><p>冷たいジュースが疲れた体に染み渡ります。心なしか疲労も取れてきた気がします。</p>`;
-        default: return "う　し　た　ぷ　に　き　あ　く　ん　笑";
-      }
+    version,
+    showJuice: false,
+    juice: {
+      apple: new Decimal(0),
     },
+    messages: [],
   },
   methods: {
     incrementMoney() {
       this.money = this.money.add(this.add.div(this.fatigue));
+      this.fatigue = this.fatigue.add(new Decimal(1).div(this.juice.apple.add(1).mul(120)));
 
       switch (this.state) {
         case "initial":
+          this.messages.push({
+            type: "success",
+            html: `<h2>おめでとうございます！</h2><p>あなたはお金を稼ぎました。</p>`,
+          });
           this.state = "first-click";
           break;
         
         case "first-click":
           if (this.money.gte(120)) {
+            this.messages.push({
+              type: "warning",
+              html: `<h2>そろそろ疲れてきましたか？</h2><p><code>buyJuice(juice);</code>でジュースを買うことができます。ジュースの価格は<code>juicePrices</code>を見てください。</p><p>今はリンゴジュースだけが売られているので、<code>buyJuice("apple");</code>でリンゴジュースを購入できます。</p>`
+            });
             this.state = "buy-juice";
-            window.buyJuice = Function("window._buyJuice = true;");
+            window.juicePrices = juicePrices;
+            juicePrices.apple = 120;
+            window.buyJuice = Function("juice = 'apple'", "window._buyJuice = juice;");
           }
           break;
         
@@ -65,11 +71,23 @@ const app = new Vue({
       }
     },
 
-    buyJuice() {
-      if (this.money.gte(120)) {
-        this.money = this.money.sub(120);
-        this.fatigue = this.fatigue.div(2);
-        if (this.state == "buy-juice") this.state = "bought-juice";
+    buyJuice(juice) {
+      this.showJuice = true;
+
+      switch (juice) {
+        case "apple":
+          if (this.money.gte(120)) {
+            this.money = this.money.sub(120);
+            this.juice.apple = this.juice.apple.add(1);
+            if (this.state == "buy-juice") {
+              this.messages.push({
+                type: "success",
+                html: `<h2>ああ、おいしい！</h2><p>冷たいジュースが疲れた体に染み渡ります。心なしか疲労も取れてきた気がします。</p>`,
+              });
+              this.state = "bought-juice";
+            }
+          }
+        default:
       }
     }
   }
@@ -79,5 +97,13 @@ setInterval(() => {
   if (window._incrementMoney) app.incrementMoney();
   window._incrementMoney = false;
 
-  if (window._buyJuice) app.buyJuice();
+  if (window._buyJuice != null) app.buyJuice(window._buyJuice);
+  window._buyJuice = null;
 }, 20);
+
+// init
+
+app.messages.push({
+  type: "primary",
+  html: `<h2>ようこそ</h2><p>DevToolsを開いて、コンソールから<code>incrementMoney();</code>を実行してください。</p>`
+});
